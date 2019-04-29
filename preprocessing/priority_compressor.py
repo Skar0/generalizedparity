de55873f6d5696461ae06eb507c1f43e4c611820
clement <clement.tamines@gmail.com>
2019-04-29 15:44:26 +0200

    Added functions for testing purposes (to be removed later)

--- preprocessing/priority_compressor.py
+++ preprocessing/priority_compressor.py
@@ -69,6 +69,60 @@ def compress_priorities_generalized(g):
 
     return g_copy
 
+def compress_priorities_generalized_testing(g):
+    """
+    Compresses the priorities in a generalized parity game.
+    This yields a new arena g in which, the priorities have been compressed for each priority function.
+    :param g: a game arena.
+    :return: a new game arena in which the priorities have been compressed.
+    """
+
+    # We work on a copy of g, which we modify
+    g_copy = copy.deepcopy(g)
+    print g.get_nbr_priority_functions()
+    print(list(range(1, g.get_nbr_priority_functions())))
+    # Compress every priority function
+    for i in range(1, g.get_nbr_priority_functions()+1):
+        compress_function_testing(g_copy, i)
+
+    return g_copy
+
+def compress_function_testing(g, i):
+    """
+    Compresses the priorities of priority function i in the game graph g.
+    Function i must be between 1 and k (the number of functions in g).
+    This modifies the game arena g.
+    :param g: a game arena, this arena is going to be modified.
+    :param i: the id of the priority function to compress.
+    :return: the game arena g in which the priorities according to function i have been compressed.
+    """
+
+    nodes = g.get_nodes_descriptors()  # Nodes from g
+
+    priorities = sorted(nodes.iteritems(), key=lambda x: x[1][i])  # Sorts nodes by priority
+    print(priorities)
+    s = len(priorities)/2
+    current = priorities[s][1][i]  # Take the first node, its information and value nbr i
+
+    if current % 2 == 0:
+        start = 0
+    else:
+        start = 1
+
+    val = priorities[s][1][i]
+
+    for elem in priorities[s:]:
+
+        if elem[1][i] % 2 == start:
+            # Replace the info for the node : keep player and priorities and change priority
+            # according to function nbr i
+            g.nodes[elem[0]] = tuple(elem[1][:i] + tuple([val]) + elem[1][i + 1:])
+        else:
+            val += 1
+            start = ((start + 1) % 2)
+            g.nodes[elem[0]] = tuple(elem[1][:i] + tuple([val]) + elem[1][i + 1:])
+
+    return g
 
 # Old version of compression for parity games, might be used for regression testing later.
 """
