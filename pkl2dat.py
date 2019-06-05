13c0b68ea0bad4489aa0279c15dc65e202226ad1
Guillermo A. Perez <gperez@Guillermos-MacBook.local>
2019-06-05 09:06:55 +0200

    have a new version of the experiments with cumulative time

--- pkl2dat.py
+++ pkl2dat.py
@@ -3,10 +3,12 @@ import pickle
 f = open("all_data.pkl", "r")
 (game_parameters, x, y, z) = pickle.load(f)
 
-algorithms = ["psol", "psolB", "psolB Buchi-coBuchi", "psolQ"]
+algorithms = ["psol", "psolB", "psolB Buchi-coBuchi", "psolQ", "psolC"]
 print("algorithms    " + "    ".join(algorithms))
 for j in range(len(z[0])):
     (n, prts) = game_parameters[j]
-    print("benchmark " + str(j + 1) +
-          "(n=" + str(n) + ",prts=" + str(prts) + ")"
-          "    " + "    ".join([format(z[i][j], "06.2f") for i in range(len(z))]))
+    print(str(j + 1) +
+          " (n=" + str(n) + ",d=" + str(prts) + ")"
+          "    " + "    ".join([format(z[i][j], "06.2f") +
+                                " (" + format(y[i][j], "07.3f") + " s)"
+                                for i in range(len(z))]))
