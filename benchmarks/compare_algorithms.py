46e0b09b91e2c79a5002f8b20dd9dbd1b1579790
clement <clement.tamines@gmail.com>
2019-06-07 11:09:39 +0200

    Added compare method for complete algorithms on LTL benchmarks

--- benchmarks/compare_algorithms.py
+++ benchmarks/compare_algorithms.py
@@ -17,6 +17,129 @@ def set_timeout(t):
 def handler(signum, frame):
     raise Exception("Timeout!")
 
+def compare_complete_algorithms_LTLbenchmarks(algorithms, generator, n, preprocess=None, iterations=3, step=10, check_solution=False,
+                                plot=False, path=" ", title="plot", labels=None):
+    """
+    Compares the running time of so called complete algorithms for parity or generalized parity games.
+    This implementation is specific to LTL benchmarks as it sorts the points before plotting them.
+    :param algorithms: list of algorithms.
+    :param generator: generator to create the games on which to run these algorithms.
+    :param n: maximum parameter for the generator.
+    :param preprocess: a pre-processing algorithm to apply on the game graphs (usually priority compression).
+    :param iterations: the number of times the solving time is recorded (minimum time is chosen).
+    :param step: the step used for the parameter of the generator.
+    :param check_solution: an algorithm to check the solutions computed for control purposes (a complete solver for the
+    given game).
+    :param plot: whether to plot the result.
+    :param path: path for the plot.
+    :param title: title for the plot.
+    :param labels: labels for the plot (name of each algorithm).
+    """
+
+    number_of_algorithms = len(algorithms)
+
+    y = [[] for z in xrange(number_of_algorithms)]
+    x = []
+
+    chrono = timer.Timer(verbose=False)  # Timer object
+
+    # Games generated are size 5 to n using the specified step
+    for i in range(0, n , step):
+
+        # if check_solution, we will verify the solutions are the same across the different algorithms
+        if check_solution:
+            winning_player_1 = []
+            winning_player_2 = []
+
+        recordings = [[0] * iterations for z in xrange(number_of_algorithms)]
+
+        g = generator(i)  # game generation
+        x.append(len(g.get_nodes()))
+
+        for k in range(number_of_algorithms):
+            g_copy = copy.deepcopy(g)
+
+            if preprocess[k] is not None:
+                g_copy = preprocess[k](g_copy)
+
+            # #iterations calls to the solver are timed
+            for j in range(iterations):
+                failed = False
+                with chrono:
+                    signal.signal(signal.SIGALRM, handler)
+                    signal.alarm(TIMEOUT)
+                    try:
+                        W1, W2 = algorithms[k](g_copy)  # solver call
+                    except Exception:
+                        failed = True
+                        print("Algorithm " + str(k) + " just timed out, benchmark "+str(i))
+                        W1, W2 = [], []  # probably a timeout
+
+                if not failed:
+                    recordings[k][j] = chrono.interval
+
+            if min(recordings[k]) == 0:
+                y[k].append(TIMEOUT)  # get the minimum out of #iterations recordings
+            else:
+                min_recording = min(recordings[k])
+                y[k].append(min_recording)  # get the minimum out of #iterations recordings
+
+            # algorithms should not modify the original game arena
+            assert (len(g.get_nodes()) == len(g_copy.get_nodes()))
+
+            if check_solution:
+                # algorithms are deterministic so we only record the last solution
+                winning_player_1.append(W1)
+                winning_player_2.append(W2)
+
+        if check_solution:
+            for u in range(number_of_algorithms - 1):
+                assert (ops.are_lists_equal(winning_player_1[u], winning_player_1[u + 1]))
+                assert (ops.are_lists_equal(winning_player_2[u], winning_player_2[u + 1]))
+
+        if plot:
+            plt.grid(True)
+            plt.title(title)
+            plt.xlabel(u'number of nodes')
+            plt.ylabel(u'time (s)')
+
+            colors = ['-g.', '-r.', '-b.', '-y.', '-c.', '-k.', '-m.']
+            plt.xscale("log")
+            plt.yscale("log")
+            from matplotlib.font_manager import FontProperties
+
+            fontP = FontProperties()
+            fontP.set_size('small')
+
+            points = []
+            for i in range(number_of_algorithms):
+                x, y[i] = (list(t) for t in zip(*sorted(zip(x, y[i])))) # sorting points
+                points.extend(plt.plot(x, y[i], colors[i], label=labels[i]))
+
+            plt.legend(loc='upper left', handles=points, prop=fontP)
+            plt.savefig(path, bbox_inches='tight')
+            plt.clf()
+            plt.close()
+
+    """
+    if plot:
+        plt.grid(True)
+        plt.title(title)
+        plt.xlabel(u'number of nodes')
+        plt.ylabel(u'time (s)')
+
+        colors = ['-g.', '-r.', '-b.', '-y.', '-c.', '-k.', '-m.']
+        # plt.yscale("log") allows logatithmic y-axis
+
+        points = []
+        for i in range(number_of_algorithms):
+            points.extend(plt.plot(x, y[i], colors[i], label=labels[i]))
+
+        plt.legend(loc='upper left', handles=points)
+        plt.savefig(path, bbox_inches='tight')
+        plt.clf()
+        plt.close()
+    """
 
 def compare_complete_algorithms(algorithms, generator, n, preprocess=None, iterations=3, step=10, check_solution=False,
                                 plot=False, path=" ", title="plot", labels=None):
